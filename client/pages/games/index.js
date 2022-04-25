ee47c9c947d0e9681f352fa7b8b2ecb99e2b9db4
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-04-25 16:16:47 +0100

    Finished working on the backButton component

---
+++ client/pages/games/index.js
@@ -0,0 +1,10 @@
+import BackButton from "../../Components/BackButton";
+
+const Games = (props) => {
+    return (
+        <main className="container pt-[23px] pb-[70px] px-[32px]">
+            <BackButton />
+        </main>
+    )
+}
+export default Games;
