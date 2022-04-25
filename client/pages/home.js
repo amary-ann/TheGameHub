5ce355772eeeb79408e94c539506858a6d543ba3
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-04-25 11:41:22 +0100

    FInished tailwind css setup and styled Nav Button

---
+++ client/pages/home.js
@@ -0,0 +1,10 @@
+import NavButton from "../Components/NavButton";
+
+const Home = (props) => {
+    return (
+        <div >
+            <NavButton />
+        </div>
+    );
+}
+export default Home;
