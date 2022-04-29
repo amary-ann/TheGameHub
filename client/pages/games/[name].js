7ed7105f7e702ea5a8868e591f98019b02ee7a3c
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-04-29 13:29:43 +0100

    Started working on the Choose opponent Page

---
+++ client/pages/games/[name].js
@@ -0,0 +1,11 @@
+import BackButton from "../../Components/BackButton";
+
+const ChooseOpponent = (props) => {
+    return (
+        <main className="container mx-auto pt-[23px] pb-[70px] px-[32px]">
+            <BackButton />
+        </main>
+    );
+}
+
+export default ChooseOpponent;
