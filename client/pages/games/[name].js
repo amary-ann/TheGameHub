3664aadd5a52b28bc0cb6cc38a4719cc6c498926
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-04-29 15:48:40 +0100

    Added icons to project and added the title of the choose opponents page

--- client/pages/games/[name].js
+++ client/pages/games/[name].js
@@ -1,9 +1,35 @@
 import BackButton from "../../Components/BackButton";
+import Head from 'next/head';
+import {useRouter} from 'next/router';
 
 const ChooseOpponent = (props) => {
+    const router = useRouter();
+    const getRouteTitleFromPath = () => {
+        const {name : gameName} = router.query;
+        switch (gameName) {
+            case 'tictactoe':
+                return 'Tic Tac Toe';
+            case 'chess': 
+                return 'Chess';
+            case 'wordsmith':
+                return 'Word Smith';
+            case 'fastestfingers':
+                return 'Fastest Fingers'
+            default :
+                return 'Game not found';
+        }
+
+    }
     return (
         <main className="container mx-auto pt-[23px] pb-[70px] px-[32px]">
+            <Head>
+                <title>{getRouteTitleFromPath()}</title>
+            </Head>
             <BackButton />
+            <h1 className="text-[26px] font-semibold poppinsFont">Choose your opponent</h1>
+            <div className="">
+
+            </div>
         </main>
     );
 }
