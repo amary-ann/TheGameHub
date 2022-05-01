5c3c05aef11dbc79d21ba9fa1e23311c9c30128b
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-05-01 15:46:50 +0100

    Finished working on the loading animation page

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,9 +1,16 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import LoadingAnimation from '../Components/LoadingAnimation'
-import styles from '../styles/Home.module.css'
+import LoadingAnimation from '../Components/LoadingAnimation';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 export default function Home() {
+  const router = useRouter();
+
+  useEffect(() => {
+
+    setTimeout(() => router.push('/home'), 2200);
+
+  }, [])
+
   return (
       <LoadingAnimation />
   )
