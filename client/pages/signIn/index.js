165fe2836fc7555cdbad7cb83a0399d4fec77187
Ronald Dosunmu <ronaldosunmu@gmail.com>
2022-05-09 22:51:24 +0100

    Extracted the OrRibbon into a component

--- client/pages/signIn/index.js
+++ client/pages/signIn/index.js
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import { useState } from 'react';
 import FormGroup from '../../Components/FormGroup';
+import OrRibbon from '../../Components/OrRibbon';
 
 const SignIn = (props) => {
     const [emailOrUsername, setEmailOrUsername] = useState('');
@@ -19,19 +20,9 @@ const SignIn = (props) => {
                 <FormGroup title="Email or username" className='mb-[23px]' value={emailOrUsername} setValue={setEmailOrUsername}/>
                 <FormGroup title="Password" className='mb-[35px]' type='password' value={password} setValue={setPassword}/>
                 <button onClick={handleSubmit} className='bg-[#4C54C8] w-full h-[62px] text-[23px] text-white rounded-[13px] font-bold'>Sign Up</button>
-                <div className='grid grid-cols-[auto_40px_auto] mt-[23px]'>
-                    <div className='flex items-center'>
-                        <div className='h-[1.5px] bg-[#4C54C8] w-full'></div>
-                    </div>
-                    <p className='text-center text-[12px] font-bold'>Or</p>
-                    <div className='flex items-center'>
-                        <div className='h-[1.5px] bg-[#4C54C8] w-full'></div>
-                    </div>
-                </div>
-                <div className="flex justify-between mt-[38px]">
-                    <div className='w-[150px] h-[87px] rounded-[27px] automaticSignUpButton  flex items-center justify-center'>
-                        <Image src='/googleIcon.svg' width={48} height={48} />
-                    </div>
+                <OrRibbon />
+                <div className="flex justify-between mt-[38px] gap-5">
+
                     <div className='w-[150px] h-[87px] rounded-[27px] automaticSignUpButton  flex items-center justify-center'>
                         <Image src='/appleIcon.svg' width={48} height={48} />
                     </div>
